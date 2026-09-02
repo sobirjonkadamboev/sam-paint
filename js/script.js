8dc9e3d64f231a93cc66db91ead0b0a804992696
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-02 04:34:28 +0000

    Auto-backup: 9/2/2026, 4:34:28 AM - Updated js/script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 const canvas = document.querySelector('canvas')
-let context = canvas.getContext('2d')
+let context = canvas.getContext('2d'),
+	isDrawing = false
 
 window.addEventListener('load', () => {
 	canvas.width = canvas.offsetWidth
 	canvas.height = canvas.offsetHeight
 })
+
+const startDraw = () => {}
 
 const drawing = e => {
 	context.lineTo(e.offsetX, e.offsetY)
