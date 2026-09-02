3a5a40765761069ee702a64446455cb1afdd905d
Sobirjon Kadamboev <sobirjonkadamboev@gmail.com>
2026-09-02 04:30:19 +0000

    Auto-backup: 9/2/2026, 4:30:19 AM - Updated js/script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ let context = canvas.getContext('2d')
 
 window.addEventListener('load', () => {
 	canvas.width = canvas.offsetWidth
+	canvas.height = canvas.offsetHeight
 })
 
 const drawing = e => {
